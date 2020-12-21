c2837edd576729070b266170106c33f19e580b78
William Beaty <31560421+wbeaty@users.noreply.github.com>
2020-12-21 01:29:04 -0500

    added phase drift write up

--- js/gif.js
+++ js/gif.js
@@ -1,5 +1,4 @@
 function loadGIF(elem){//quick and dirty way to load a gif on click, by encoding the GIF name into the id
-    console.log(elem.id);
     if (!elem.src.includes(elem.id))//dont reload one double click
         elem.src=elem.id;
 }
